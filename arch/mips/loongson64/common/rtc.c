0a399c1fa87a76334093d7cc26c8e07eef8a970b
Jiaxun Yang <jiaxun.yang@flygoat.com>
2017-12-26 20:53:55 +0800

    MIPS: Loongson64: cleanup all loongson common files to use SPDX Identifier
     To reduce unnecessary license text.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- arch/mips/loongson64/common/rtc.c
+++ arch/mips/loongson64/common/rtc.c
@@ -1,12 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
 /*
- *  Lemote Fuloong platform support
+ *  mc146818 rtc support for lemote-2f platform
  *
  *  Copyright(c) 2010 Arnaud Patard <apatard@mandriva.com>
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
  */
 
 #include <linux/init.h>
--- arch/mips/loongson64/common/time.c
+++ arch/mips/loongson64/common/time.c
@@ -1,3 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
 /*
  * Copyright (C) 2007 Lemote, Inc. & Institute of Computing Technology
  * Author: Fuxin Zhang, zhangfx@lemote.com
@@ -5,10 +7,6 @@
  * Copyright (C) 2009 Lemote Inc.
  * Author: Wu Zhangjin, wuzhangjin@gmail.com
  *
- *  This program is free software; you can redistribute	 it and/or modify it
- *  under  the terms of	 the GNU General  Public License as published by the
- *  Free Software Foundation;  either version 2 of the	License, or (at your
- *  option) any later version.
  */
 #include <asm/mc146818-time.h>
 #include <asm/time.h>
