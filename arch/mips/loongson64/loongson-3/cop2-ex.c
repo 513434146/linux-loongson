9d860fde7619c430b64cd32f545b1d70af2e2c79
Jiaxun Yang <jiaxun.yang@flygoat.com>
2017-12-26 21:06:12 +0800

    MIPS: Loongson64: cleanup all loongson-3 files to use SPDX Identifier
    To reduce unnecessary license text.
    
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>

--- arch/mips/loongson64/loongson-3/cop2-ex.c
+++ arch/mips/loongson64/loongson-3/cop2-ex.c
@@ -1,7 +1,6 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
 /*
- * This file is subject to the terms and conditions of the GNU General Public
- * License.  See the file "COPYING" in the main directory of this archive
- * for more details.
  *
  * Copyright (C) 2014 Lemote Corporation.
  *   written by Huacai Chen <chenhc@lemote.com>
